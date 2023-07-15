37054361ee762458d214d3dfd595200cbab4127e
Ana Laura <6030.anaalbuquerque@gmail.com>
2023-07-15 12:12:27 -0300

    adicionado clear e delay

--- kernel.asm
+++ kernel.asm
@@ -3,43 +3,152 @@ jmp 0x0000:start
 
 
 data:
-	string db "00000",0
+    string db "00000",0
     resposta db "navio",0
-    resposta_aux db "navio", 0
+    
     char0_aux db 0
     char1_aux db 0
     char2_aux db 0
     char3_aux db 0
     char4_aux db 0
-
+    
+    termoo db "TERMOO", 0
+    
     verde db 10
     roxo db 5
     branco db 15
     asterisco db 42
+    
+    flag db 0
+    linha db 0
+    coluna db 17
+    venceu db 0
+    derrota db "PERDEU TUDO!", 0
+
+
+    a_green db 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+            db 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+            db 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 
+            db 11, 11, 11, 11, 11, 11, 15,  0,  0,  0,  0,  0,  0, 11, 11, 11, 
+            db 11, 11, 11, 11, 11, 15,  0,  0,  0,  0,  0,  0,  0,  0, 11, 11, 
+            db 11, 11, 11, 11, 11, 15,  0,  0, 11, 11, 11, 15,  0,  0, 11, 11, 
+            db 11, 11, 11, 11, 11, 15,  0,  0, 11, 11, 11, 15,  0,  0, 11, 11, 
+            db 11, 11, 11, 11, 11, 15,  0,  0, 15, 15, 15, 15,  0,  0, 11, 11,
+            db 11, 11, 11, 11, 11, 15,  0,  0,  0,  0,  0,  0,  0,  0, 11, 11, 
+            db 11, 11, 11, 11, 11, 15,  0,  0,  0,  0,  0,  0,  0,  0, 11, 11, 
+            db 11, 11, 11, 11, 11, 15,  0,  0, 11, 11, 11, 15,  0,  0, 11, 11, 
+            db 11, 11, 11, 11, 11, 15,  0,  0, 11, 11, 11, 15,  0,  0, 11, 11, 
+            db 11, 11, 11, 11, 11, 15,  0,  0, 11, 11, 11, 15,  0,  0, 11, 11, 
+            db 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 
+            db 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 
+            db 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 
 
 start:
     xor ax, ax
-    xor dx, dx 
+    xor dx, dx  
     mov ds, ax
     mov es, ax
-    
+
     call modoVideo
-    call looping 
+
+    mov dh, 4
+    mov dl, 17
+    call setcursor
+
+    call termo
     
+    call getchar
+
+    cmp al, 49
+    jne start
+
+    xor dx, dx
+    xor ax, ax
+    call clear
+    call looping
+
+    mov dh, 10
+    mov dl, 13
+    call setcursor
+
+    mov si, derrota
+    call prints
+
+    call getchar
+
     call fim
+
+
+termo:
+    call delay
+    mov al, 'T'
+    call printchar
+
+    call delay
+    mov al, 'E'
+    call printchar
+
+    call delay
+    mov al, 'R'
+    call printchar
+
+    call delay
+    mov al, 'M'
+    call printchar
+
+    call delay
+    mov al, 'O'
+    call printchar
+    ret
+
+
+loop_letras:
+
+    mov dx, 0
+    mov cx, 0
+    inc dx
+    cmp dx, 16
+    je .done
+
+    .loop1:
+
+        lodsb
+        mov ah, 0ch
+        inc cx
+        int 10h
+        cmp cx, 16
+        je loop_letras
+        jmp .loop1
+
+    .done:
+        jmp $
+
 
 looping: 
     .loop:
-        mov di, string	
-	    mov bl, 15
+
+        inc byte[linha]
+        mov byte[coluna], 17
+        mov di, string  
+        mov bl, 15
         
         push dx
+
+        mov dl, 17
+        mov dh, byte[linha]
+        call setcursor
+
         call gets
+        call endl
+
+        inc byte[flag]
+
         pop dx
         inc dx
-        
+
         push dx
         
+
         call search_green
         call search_purple
         
@@ -53,6 +162,7 @@ looping:
 
         cmp dx, 7
         jne .loop
+
     ret 
 
 modoVideo:
@@ -61,14 +171,39 @@ modoVideo:
     int 10h
 
     ;letra maior
-    mov ah, 0xb
-    mov al, 0
+    mov ah, 11h
+    mov al, 03h
+
+    mov ah, 4Ch
+    int 21h
 
     mov bh, 0
     mov bl, 15 ;cor
     int 10h
     
     ret
+
+clear:                
+    ; set the cursor to top left-most corner of screen
+    mov dx, 0 
+    mov bh, 0      
+    mov ah, 0x2
+    int 0x10
+
+    ; print 2000 blank chars to clean  
+    mov cx, 2000 
+    mov bh, 0
+    mov al, 0x20 ; blank char
+    mov ah, 0x9
+    int 0x10
+    
+    ; reset cursor to top left-most corner of screen
+    mov dx, 0 
+    mov bh, 0      
+    mov ah, 0x2
+    int 0x10
+    ret
+
 
 gets:
     xor cx, cx ;contador = 0
@@ -79,13 +214,27 @@ gets:
         cmp al,0x08
         je .backspace
 
-        inc cx
-        stosb
-        call printchar
         cmp al, 0x0d ;0x0d = 13 = enter
-        je .done
+        je .pre_done
 
-        jmp .loop
+        jmp .checa_limite
+
+        .pre_done:
+            cmp cx, 5
+            je .done
+
+            jmp .loop
+            
+        .checa_limite:
+
+            cmp cx, 5
+                je .loop
+            
+            inc cx
+            stosb
+
+            call printchar
+            jmp .loop
 
         .backspace:
             cmp cl,0
@@ -98,10 +247,9 @@ gets:
             
         .done:
             call printchar
-            dec di
             mov al, 0
             stosb
-            ;call endl
+
             ret
 
 prints:
@@ -145,6 +293,7 @@ endl:
     ret
 
 search_green:
+
     mov si, string     ;caracteres em al
     mov dx, 0           ;dx como indice de string
 
@@ -152,6 +301,7 @@ search_green:
     mov cx, 0           ;cx como indice de resposta
 
     .loop:
+
         lodsb
         cmp dx, 5
         je .end
@@ -166,9 +316,6 @@ search_green:
             ret
         
         .green:
-            ;mov bh, 0 
-            ;mov bl, [verde]
-            ;call printchar
 
             cmp cx, 0
             je .char0
@@ -237,7 +384,19 @@ compare_char:
     .endcomp:           ;se acabou e não printou letra verde ou 
         mov bh, 0       ;amarelo, printa a letra branca
         mov bl, [branco] 
+
+        push dx
+        xor dx, dx
+        mov dl, byte[coluna]
+        mov dh, byte[linha]
+        call setcursor
+
         call printchar
+
+        xor dx, dx
+        inc byte[coluna]
+        pop dx
+        
         ret
     
     .rodaloop:
@@ -266,46 +425,77 @@ compare_char:
             cmp byte[char0_aux], 42  ;se for * é pq é verde e já foi colocado
             je .rodaloop
             mov byte[char0_aux], 42
+            ;push dx
             jmp .purple
         
         .checkasteristico1:
             cmp byte[char1_aux], 42  ;se for * é pq é verde e já foi colocado
             je .rodaloop
             mov byte[char1_aux], 42
+            ;push dx
             jmp .purple
         
         .checkasteristico2:
             cmp byte[char2_aux], 42  ;se for * é pq é verde e já foi colocado
             je .rodaloop
             mov byte[char2_aux], 42
+            ;push dx
             jmp .purple
         
         .checkasteristico3:
             cmp byte[char3_aux], 42  ;se for * é pq é verde e já foi colocado
             je .rodaloop
             mov byte[char3_aux], 42
+            ;push dx
             jmp .purple
         
         .checkasteristico4:
             cmp byte[char4_aux], 42  ;se for * é pq é verde e já foi colocado
             je .rodaloop
             mov byte[char4_aux], 42
+            ;push dx
             jmp .purple
 
 
         .purple:
             mov bh, 0
-            mov bl, [roxo] 
+            mov bl, [roxo]
+            
+            push dx
+            xor dx, dx
+            mov dl, byte[coluna]
+            mov dh, byte[linha]
+            call setcursor
+
             call printchar
+            inc byte[coluna]
+            pop dx
+
             ret
 
         .green:
             mov bh, 0
             mov bl, [verde]
+
+            push dx
+            xor dx, dx
+            mov dl, byte[coluna]
+            mov dh, byte[linha]
+            call setcursor
+
             call printchar
+            inc byte[coluna]
+            pop dx
+
             ret
 
          
+delay:
+   mov ah, 86h
+   mov cx,5
+   mov dx,500
+   int 15h
+ret
 
 strcpy:
     .loop1:
@@ -327,7 +517,3 @@ setcursor:
 
 fim:
     jmp $
-
-
-times 510 - ($ - $$) db 0
-dw 0xaa55 ; assinatura de boot
