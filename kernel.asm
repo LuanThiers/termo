d790c47db94c7e5b9bb5991886bf0d86383dc074
Ana Laura <6030.anaalbuquerque@gmail.com>
2023-07-16 09:39:50 -0300

    adicionado tentativas

--- kernel.asm
+++ kernel.asm
@@ -23,29 +23,40 @@ data:
     linha db 0
     coluna db 17
     venceu db 0
-    derrota db "PERDEU TUDO!", 0
+    derrota_msg db "PERDEU TUDO!", 0
+    vitoria_msg db "GANHOU TUDO!", 0
+    tentativas_msg db "Tentativas: ", 0
+    inicio_msg db "1 - START", 0
+    sair_msg db "2 - SAIR", 0
+    tutorial_msg db "3 - TUTORIAL", 0
 
     words db "anzol", 0, "beijo", 0, "curva", 0, "dardo", 0
     number_words dw 4
     lenght_words db 6 ;5 letras + 0
     ticks db 0
+    
+    aviso_roxo db 0
+    coluna_roxa db 0
+    contador_verde db 0
+    coluna_entrada db 0
+    tentativas db 55 ;corresponde a 7 na tabela asc
 
     a_green db 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
             db 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
-            db 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 
-            db 11, 11, 11, 11, 11, 11, 15,  0,  0,  0,  0,  0,  0, 11, 11, 11, 
-            db 11, 11, 11, 11, 11, 15,  0,  0,  0,  0,  0,  0,  0,  0, 11, 11, 
-            db 11, 11, 11, 11, 11, 15,  0,  0, 11, 11, 11, 15,  0,  0, 11, 11, 
-            db 11, 11, 11, 11, 11, 15,  0,  0, 11, 11, 11, 15,  0,  0, 11, 11, 
-            db 11, 11, 11, 11, 11, 15,  0,  0, 15, 15, 15, 15,  0,  0, 11, 11,
-            db 11, 11, 11, 11, 11, 15,  0,  0,  0,  0,  0,  0,  0,  0, 11, 11, 
-            db 11, 11, 11, 11, 11, 15,  0,  0,  0,  0,  0,  0,  0,  0, 11, 11, 
-            db 11, 11, 11, 11, 11, 15,  0,  0, 11, 11, 11, 15,  0,  0, 11, 11, 
-            db 11, 11, 11, 11, 11, 15,  0,  0, 11, 11, 11, 15,  0,  0, 11, 11, 
-            db 11, 11, 11, 11, 11, 15,  0,  0, 11, 11, 11, 15,  0,  0, 11, 11, 
-            db 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 
-            db 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 
-            db 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 
+            db 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+            db 11, 11, 11, 11, 11, 15,  0,  0,  0,  0,  0,  0, 11, 11, 11, 11,
+            db 11, 11, 11, 11, 15,  0,  0,  0,  0,  0,  0,  0,  0, 11, 11, 11,
+            db 11, 11, 11, 11, 15,  0,  0, 11, 11, 11, 15,  0,  0, 11, 11, 11,
+            db 11, 11, 11, 11, 15,  0,  0, 11, 11, 11, 15,  0,  0, 11, 11, 11,
+            db 11, 11, 11, 11, 15,  0,  0, 15, 15, 15, 15,  0,  0, 11, 11, 11,
+            db 11, 11, 11, 11, 15,  0,  0,  0,  0,  0,  0,  0,  0, 11, 11, 11,
+            db 11, 11, 11, 11, 15,  0,  0,  0,  0,  0,  0,  0,  0, 11, 11, 11,
+            db 11, 11, 11, 11, 15,  0,  0, 11, 11, 11, 15,  0,  0, 11, 11, 11,
+            db 11, 11, 11, 11, 15,  0,  0, 11, 11, 11, 15,  0,  0, 11, 11, 11,
+            db 11, 11, 11, 11, 15,  0,  0, 11, 11, 11, 15,  0,  0, 11, 11, 11,
+            db 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+            db 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+            db 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
 
 start:
     xor ax, ax
@@ -60,27 +71,15 @@ start:
     call setcursor
     call termo
     
+
+    mov dh, 10
+    mov dl, 17
+    call setcursor
+
     call getchar
 
     cmp al, 49
-    jne start
-
-    xor dx, dx
-    xor ax, ax
-    call clear
-    call define_word
-    call looping
-
-    mov dh, 10
-    mov dl, 13
-    call setcursor
-
-    mov si, derrota
-    call prints
-
-    call getchar
-
-    call fim
+    je jogo
 
 
 termo:
@@ -103,12 +102,35 @@ termo:
     call delay
     mov al, 'O'
     call printchar
+
+    call delay
+
+    mov dh, 18
+    mov dl, 2
+    call setcursor
+    
+    mov si, inicio_msg
+    call prints
+
+    mov dh, 18
+    mov dl, 14
+    call setcursor
+
+    mov si, sair_msg
+    call prints
+
+    mov dh, 18
+    mov dl, 26
+    call setcursor
+
+    mov si, tutorial_msg
+    call prints
+
     ret
 
 
 loop_letras:
 
-    mov dx, 0
     mov cx, 0
     inc dx
     cmp dx, 16
@@ -125,17 +147,42 @@ loop_letras:
         jmp .loop1
 
     .done:
-        jmp $
+        ret
 
+
+jogo:
+    xor dx, dx
+    xor ax, ax
+    call clear
+    call define_word
+    call looping
+    ret
 
 looping: 
     call clear_reg
+
+    mov dl, 25
+    mov dh, 1
+    call setcursor
+    mov si, tentativas_msg
+    call prints
+
+
     .loop:
+        ;setando o cursor pra printar tentativas
+        mov dl, 37
+        mov dh, 1
+        call setcursor
+
+        ;printa tentativas
+        mov bl, [branco]
+        mov al, byte[tentativas]
+        call printchar
+        dec byte[tentativas]
 
         inc byte[linha]
         mov byte[coluna], 17
-        mov di, string  
-        mov bl, 15
+        mov di, string          ;string do usuário
         
         push dx
 
@@ -146,29 +193,52 @@ looping:
         call gets
         call endl
 
-        inc byte[flag]
+        ;inc byte[flag]
 
         pop dx
         inc dx
 
         push dx
-        
-
         call search_green
         call search_purple
-        
         pop dx 
-        
-        mov byte[char0_aux], 0
-        mov byte[char1_aux], 0
-        mov byte[char2_aux], 0
-        mov byte[char3_aux], 0
-        mov byte[char4_aux], 0
 
-        cmp dx, 7
+        call delay ;para mostrar o resultado da última tentativa
+
+        cmp byte[contador_verde], 5
+        je vitoria
+
+        call reint_flags    ;reiniciando flags 
+        cmp byte[tentativas], 48 ;se for 0, perdeu e vai pra derrota
         jne .loop
+        jmp derrota
 
     ret 
+
+vitoria:
+    call clear
+    mov dh, 10
+    mov dl, 13
+    call setcursor
+
+    mov si, vitoria_msg
+    call prints
+
+    call getchar
+    jmp fim
+
+derrota:
+    call clear
+    mov dh, 10
+    mov dl, 13
+    call setcursor
+
+    mov bl, [roxo]
+    mov si, derrota_msg
+    call prints
+
+    call getchar
+    jmp fim
 
 modoVideo:
     mov ah, 0
@@ -207,6 +277,7 @@ clear:
 
 
 gets:
+    mov byte[coluna_entrada], 17
     xor cx, cx ;contador = 0
     mov al,0
     .loop:
@@ -234,7 +305,12 @@ gets:
             inc cx
             stosb
 
+            mov dh, 15
+            mov dl, byte[coluna_entrada]
+            call setcursor
+
             call printchar
+            inc byte[coluna_entrada]
             jmp .loop
 
         .backspace:
@@ -243,14 +319,27 @@ gets:
             dec di
             dec cl
             mov byte[di],0
+            dec byte[coluna_entrada]
             call delchar
             jmp .loop
             
         .done:
+
+            call delchar
+            call delchar
+            call delchar
+            call delchar
+            call delchar
+
+            mov dh, 15
+            mov dl, byte[coluna_entrada]
+            call setcursor
             call printchar
+            inc byte[coluna_entrada]
             mov al, 0
             stosb
 
+            
             ret
 
 prints:
@@ -383,6 +472,10 @@ compare_char:
     
 
     .endcomp:           ;se acabou e não printou letra verde ou 
+
+        cmp byte[aviso_roxo], 1
+        je .retorno
+
         mov bh, 0       ;amarelo, printa a letra branca
         mov bl, [branco] 
 
@@ -400,11 +493,14 @@ compare_char:
         
         ret
     
+    .retorno:
+        mov byte[aviso_roxo], 0
+        ret
+
     .rodaloop:
         inc di            ;próximo caracter de string
         inc cx            ;incrementa contador de resposta
         jmp compare_char 
-
 
 
     .definecolor:
@@ -426,39 +522,41 @@ compare_char:
             cmp byte[char0_aux], 42  ;se for * é pq é verde e já foi colocado
             je .rodaloop
             mov byte[char0_aux], 42
-            ;push dx
             jmp .purple
         
         .checkasteristico1:
             cmp byte[char1_aux], 42  ;se for * é pq é verde e já foi colocado
             je .rodaloop
             mov byte[char1_aux], 42
-            ;push dx
             jmp .purple
         
         .checkasteristico2:
             cmp byte[char2_aux], 42  ;se for * é pq é verde e já foi colocado
             je .rodaloop
             mov byte[char2_aux], 42
-            ;push dx
             jmp .purple
         
         .checkasteristico3:
             cmp byte[char3_aux], 42  ;se for * é pq é verde e já foi colocado
             je .rodaloop
             mov byte[char3_aux], 42
-            ;push dx
             jmp .purple
         
         .checkasteristico4:
             cmp byte[char4_aux], 42  ;se for * é pq é verde e já foi colocado
             je .rodaloop
             mov byte[char4_aux], 42
-            ;push dx
             jmp .purple
 
 
         .purple:
+
+            cmp byte[aviso_roxo], 0
+            je .printar_purple
+            jmp .rodaloop
+
+        .printar_purple:
+
             mov bh, 0
             mov bl, [roxo]
             
@@ -467,16 +565,21 @@ compare_char:
             mov dl, byte[coluna]
             mov dh, byte[linha]
             call setcursor
-
             call printchar
+
             inc byte[coluna]
             pop dx
 
-            ret
+            mov byte[aviso_roxo], 1
+
+            jmp .rodaloop  
 
         .green:
             mov bh, 0
             mov bl, [verde]
+
+            cmp byte[aviso_roxo], 1
+            je .letra_verde_dupla
 
             push dx
             xor dx, dx
@@ -488,16 +591,32 @@ compare_char:
             inc byte[coluna]
             pop dx
 
+            inc byte[contador_verde]
+
             ret
 
+        .letra_verde_dupla:
+            
+            mov byte[aviso_roxo], 0
+            push dx
+            xor dx, dx
+            dec byte[coluna]
+            mov dl, byte[coluna]
+            mov dh, byte[linha]
+            call setcursor
+
+            call printchar
+            inc byte[coluna]
+            pop dx
+
+            ret
          
 delay:
    mov ah, 86h
    mov cx,5
-   mov dx,500
+   mov dx,400
    int 15h
-ret
-
+    ret
 
 setcursor:
     mov ah, 2
@@ -563,6 +682,15 @@ clear_reg:
     xor ax, ax
     xor dx, dx
     xor cx, cx
+    ret
+
+reint_flags:
+    mov byte[char0_aux], 0
+    mov byte[char1_aux], 0
+    mov byte[char2_aux], 0
+    mov byte[char3_aux], 0
+    mov byte[char4_aux], 0
+    mov byte[contador_verde], 0
     ret
 
 
